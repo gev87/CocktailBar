f6d451370591312b598a78e323047fbfebe61571
g-zakaryan <garnikzakaryan89@gmail.com>
2022-04-14 12:38:20 +0400

    new2

--- cocktailbuildershop/src/components/Demo.js
+++ cocktailbuildershop/src/components/Demo.js
@@ -5,12 +5,12 @@ import MainContext from "../context/MainContext";
 // import Customer from "./Customer";
 import NavBar from "./NavBar"
 
-
 export default function Demo() {
 	const { currentUser } = useContext(MainContext)
 	return (
 		<>
 			<NavBar/>
+			
 			Demo (main page without authorization)
 			{/* {currentUser ? <Customer /> : <div><Link to="/login">Log in</Link>{"  or  "} 	<Link to="/signup">Sign up</Link> </div>} */}
 		</>
