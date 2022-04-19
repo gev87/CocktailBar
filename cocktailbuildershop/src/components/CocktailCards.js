e3de703a1841bee8a3106548ef157e0853ee11ea
g-zakaryan <garnikzakaryan89@gmail.com>
2022-04-19 14:32:54 +0400

    footer

--- cocktailbuildershop/src/components/CocktailCards.js
+++ cocktailbuildershop/src/components/CocktailCards.js
@@ -44,10 +44,6 @@ const useStyles = makeStyles((theme) => ({
 	cardContent: {
 		flexGrow: 1,
 	},
-	footer: {
-		backgroundColor: theme.palette.background.paper,
-		padding: theme.spacing(6),
-	},
 }));
 
 //const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -137,22 +133,6 @@ export default function CocktailCards() {
 					</Grid>
 				</Container>
 			</main>
-			{/* Footer */}
-			<footer className={classes.footer}>
-				{/* <Typography variant="h6" align="center" gutterBottom>
-					Footer
-				</Typography>
-				<Typography
-					variant="subtitle1"
-					align="center"
-					color="textSecondary"
-					component="p"
-				>
-					Something here to give the footer a purpose!
-				</Typography> */}
-				<Copyright />
-			</footer>
-			{/* End footer */}
 		</React.Fragment>
 	);
 }
