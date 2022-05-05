52fe220016d2f587ddca36456d2d5a3b7d9dfacf
Samvel Tadevosyan <63152210+satadevosyan@users.noreply.github.com>
2022-05-05 23:52:31 +0400

    Fix firebase read (#20)
    * fix
    
    * fix cart qty bug

--- cocktailbuildershop/src/components/CocktailBuilder.js
+++ cocktailbuildershop/src/components/CocktailBuilder.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Container, Typography, Button, TextField } from "@material-ui/core";
 import NavBar from "./NavBar";
 import Autocomplete from "@material-ui/lab/Autocomplete";
@@ -11,6 +11,7 @@ import ShopIcon from "@material-ui/icons/Shop";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import Footer from "./Footer";
 import THEMES from "../consts/THEMES";
+import { calcItemQty } from "../utils/Commonfuncs";
 import {
   writeAsync,
   readOnceGet,
@@ -24,10 +25,16 @@ function CustomCocktail() {
   const [ingridient3, setIngridient3] = useState("");
   const [ingridient4, setIngridient4] = useState("");
   const [cocktailName, setCocktailName] = useState("");
+  const [cartQty, setCartQty] = useState(null);
+  const [cartChanged, setCartChanged] = useState(null);
   const [error, setError] = useState("");
   const { currentUser } = useContext(MainContext);
   const classes = THEMES();
-  	const navigate = useNavigate();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    currentUser && setCartQty(calcItemQty(currentUser));
+  }, [currentUser, cartChanged]);
 
   let obj = {
     idDrink:
@@ -68,6 +75,8 @@ function CustomCocktail() {
               });
         }
       );
+    setCartQty(cartQty + 1);
+    setCartChanged([]);
   };
 
   function handleSubmit() {
@@ -97,153 +106,153 @@ function CustomCocktail() {
   }
 
   return (
-		<div>
-			<Container maxWidth="sm">
-				{error[2] === "n" ? (
-					<div>
-						<Alert variant="filled">{error}</Alert>
-					</div>
-				) : (
-					error && (
-						<Alert variant="filled" severity="error">
-							{error}
-						</Alert>
-					)
-				)}
-			</Container>
-			<div
-				style={{
-					display: "flex",
-					minWidth: "300px",
-					paddingLeft: "300px",
-					flexDirection: "row",
-					flexWrap: "wrap",
-				}}
-			>
-				<div style={{ paddingTop: "20px" }}>
-					<TextField
-						inputProps={{ className: classes.input }}
-						onChange={(e) => setCocktailName(e.target.value)}
-						variant="outlined"
-						label="COCKTAIL NAME"
-						value={cocktailName}
-						style={{ width: 250, padding: "15px 0px" }}
-						color="secondary"
-					/>
+    <div>
+      <NavBar cartQty={cartQty} />
+      <Container maxWidth="sm">
+        {error[2] === "n" ? (
+          <div>
+            <Alert variant="filled">{error}</Alert>
+          </div>
+        ) : (
+          error && (
+            <Alert variant="filled" severity="error">
+              {error}
+            </Alert>
+          )
+        )}
+      </Container>
+      <div
+        style={{
+          display: "flex",
+          minWidth: "300px",
+          paddingLeft: "300px",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        <div style={{ paddingTop: "20px" }}>
+          <TextField
+            inputProps={{ className: classes.input }}
+            onChange={(e) => setCocktailName(e.target.value)}
+            variant="outlined"
+            label="COCKTAIL NAME"
+            value={cocktailName}
+            style={{ width: 250, padding: "15px 0px" }}
+            color="secondary"
+          />
 
-					<Autocomplete
-						classes={{ inputRoot: classes.input }}
-						onInputChange={(event, newInputvalue) => {
-							setIngridient1(newInputvalue);
-						}}
-						inputValue={ingridient1}
-						id="controllable-states-demo"
-						options={PRICESARR}
-						getOptionLabel={(option) => option.ingridient}
-						style={{ width: 250, padding: "15px 0px" }}
-						renderInput={(params) => (
-							<TextField
-								{...params}
-								label="INGRIDIENT 1"
-								variant="outlined"
-								color="secondary"
-							/>
-						)}
-					/>
-					<Autocomplete
-						classes={{ inputRoot: classes.input }}
-						inputValue={ingridient2}
-						onInputChange={(event, newInputvalue) => {
-							setIngridient2(newInputvalue);
-						}}
-						id="controllable-states-demo"
-						options={PRICESARR}
-						getOptionLabel={(option) => option.ingridient}
-						style={{ width: 250, padding: "15px 0px" }}
-						renderInput={(params) => (
-							<TextField
-								{...params}
-								label="INGRIDIENT 2"
-								variant="outlined"
-								color="secondary"
-							/>
-						)}
-					/>
-					<Autocomplete
-						classes={{ inputRoot: classes.input }}
-						inputValue={ingridient3}
-						onInputChange={(event, newInputvalue) => {
-							setIngridient3(newInputvalue);
-						}}
-						id="controllable-states-demo"
-						options={PRICESARR}
-						getOptionLabel={(option) => option.ingridient}
-						style={{ width: 250, padding: "15px 0px" }}
-						renderInput={(params) => (
-							<TextField {...params} label="INGRIDIENT 3" variant="outlined" />
-						)}
-					/>
-					<Autocomplete
-						classes={{ inputRoot: classes.input }}
-						inputValue={ingridient4}
-						onInputChange={(event, newInputvalue) => {
-							setIngridient4(newInputvalue);
-						}}
-						id="controllable-states-demo"
-						options={PRICESARR}
-						getOptionLabel={(option) => option.ingridient}
-						style={{ width: 250, padding: "15px 0px" }}
-						renderInput={(params) => (
-							<TextField {...params} label="INGRIDIENT 4" variant="outlined" />
-						)}
-					/>
-				</div>
-				<img
-					width="30%"
-					style={{ padding: "0px 50px" }}
-					src="/images/cocktail1.jpg"
-					alt="custom"
-				/>
+          <Autocomplete
+            classes={{ inputRoot: classes.input }}
+            onInputChange={(event, newInputvalue) => {
+              setIngridient1(newInputvalue);
+            }}
+            inputValue={ingridient1}
+            id="controllable-states-demo"
+            options={PRICESARR}
+            getOptionLabel={(option) => option.ingridient}
+            style={{ width: 250, padding: "15px 0px" }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="INGRIDIENT 1"
+                variant="outlined"
+                color="secondary"
+              />
+            )}
+          />
+          <Autocomplete
+            classes={{ inputRoot: classes.input }}
+            inputValue={ingridient2}
+            onInputChange={(event, newInputvalue) => {
+              setIngridient2(newInputvalue);
+            }}
+            id="controllable-states-demo"
+            options={PRICESARR}
+            getOptionLabel={(option) => option.ingridient}
+            style={{ width: 250, padding: "15px 0px" }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="INGRIDIENT 2"
+                variant="outlined"
+                color="secondary"
+              />
+            )}
+          />
+          <Autocomplete
+            classes={{ inputRoot: classes.input }}
+            inputValue={ingridient3}
+            onInputChange={(event, newInputvalue) => {
+              setIngridient3(newInputvalue);
+            }}
+            id="controllable-states-demo"
+            options={PRICESARR}
+            getOptionLabel={(option) => option.ingridient}
+            style={{ width: 250, padding: "15px 0px" }}
+            renderInput={(params) => (
+              <TextField {...params} label="INGRIDIENT 3" variant="outlined" />
+            )}
+          />
+          <Autocomplete
+            classes={{ inputRoot: classes.input }}
+            inputValue={ingridient4}
+            onInputChange={(event, newInputvalue) => {
+              setIngridient4(newInputvalue);
+            }}
+            id="controllable-states-demo"
+            options={PRICESARR}
+            getOptionLabel={(option) => option.ingridient}
+            style={{ width: 250, padding: "15px 0px" }}
+            renderInput={(params) => (
+              <TextField {...params} label="INGRIDIENT 4" variant="outlined" />
+            )}
+          />
+        </div>
+        <img
+          width="30%"
+          style={{ padding: "0px 50px" }}
+          src="/images/cocktail1.jpg"
+          alt="custom"
+        />
 
-				<div style={{ padding: "0px 50px", width: "150px" }}>
-					<Button
-						style={{ height: "150px", background: "#ec0000", color: "#6be909" }}
-						variant="contained"
-						fullWidth
-						onClick={handleSubmit}
-					>
-						ADD To Cart
-						<ShoppingCartIcon fontSize="large" />
-					</Button>
-					<Button
-						variant="contained"
-						fullWidth
-						style={{ height: "150px", background: "yellow", color: "#6e280b" }}
-						onClick={onClean}
-					>
-						CLEAR ALL FIELDS
-						<HighlightOffIcon fontSize="large" />
-					</Button>
-					<Button
-						onClick={() => navigate("/payment")}
-						color="primary"
-						variant="contained"
-						fullWidth
-						style={{ height: "150px" }}
-					>
-						ORDER NOW <ShopIcon fontSize="large" />
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
+        <div style={{ padding: "0px 50px", width: "150px" }}>
+          <Button
+            style={{ height: "150px", background: "#ec0000", color: "#6be909" }}
+            variant="contained"
+            fullWidth
+            onClick={handleSubmit}
+          >
+            ADD To Cart
+            <ShoppingCartIcon fontSize="large" />
+          </Button>
+          <Button
+            variant="contained"
+            fullWidth
+            style={{ height: "150px", background: "yellow", color: "#6e280b" }}
+            onClick={onClean}
+          >
+            CLEAR ALL FIELDS
+            <HighlightOffIcon fontSize="large" />
+          </Button>
+          <Button
+            onClick={() => navigate("/payment")}
+            color="primary"
+            variant="contained"
+            fullWidth
+            style={{ height: "150px" }}
+          >
+            ORDER NOW <ShopIcon fontSize="large" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default function CocktailBuilder() {
   const classes = THEMES();
 
   return (
     <div>
-      <NavBar />
       <div className={classes.heroContent}>
         <Container maxWidth="sm">
           <Typography
--- cocktailbuildershop/src/components/NavBar.js
+++ cocktailbuildershop/src/components/NavBar.js
@@ -6,259 +6,234 @@ import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import SearchIcon from "@material-ui/icons/Search";
 import MainContext from "../context/MainContext";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import MenuDrawer from "./MenuDrawer";
 import HomeIcon from "@material-ui/icons/Home";
 import THEMES from "../consts/THEMES";
-import { readOnValue } from "../firebase/crudoperations";
 
-export default function MenuAppBar({ popularIngsSwitch, popularCocktailsSwitch, basketQty, fetchData, showDrawer = true }) {
-	const classes = THEMES();
-	const [anchorEl, setAnchorEl] = useState(null);
-	const open = Boolean(anchorEl);
-	const [, setError] = useState("");
-	const { currentUser, logout } = useContext(MainContext);
-	const navigate = useNavigate();
-	const [openMenu, setOpenMenu] = useState(false);
-	const [, setBasketQty] = useState();
-	// const [showDrawer, setShowDrawer] = useState(true)
+export default function MenuAppBar({
+  popularIngsSwitch,
+  popularCocktailsSwitch,
+  cartQty,
+  fetchData,
+  showDrawer = true,
+}) {
+  const classes = THEMES();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const [, setError] = useState("");
+  const { currentUser, logout } = useContext(MainContext);
+  const navigate = useNavigate();
+  const [openMenu, setOpenMenu] = useState(false);
 
-	useEffect(() => {
-		const qty =
-			currentUser &&
-			readOnValue(
-				`users/${currentUser.uid}/orders`,
-				(value) =>
-					value &&
-					Object.entries(value).reduce(
-						(prev, curr) => prev + curr[1].quantity,
-						0
-					)
-			);
-		setBasketQty(qty);
-	},[currentUser]);
-	// useEffect(() => {
-	// 	const qty =
-	// 		currentUser &&
-	// 		readOnValue(
-	// 			`users/${currentUser.uid}/orders`,
-	// 			(value) =>
-	// 				value &&
-	// 				Object.entries(value).reduce(
-	// 					(prev, curr) => prev + curr[1].quantity,
-	// 					0
-	// 				)
-	// 		);
-	// 	setBasketQty(qty);
-	// }, [currentUser]);
-	
-	const handleMenu = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	async function handleLogout() {
-		setError("");
+  async function handleLogout() {
+    setError("");
 
-		try {
-			await logout();
-			navigate("/");
-		} catch {
-			setError("Failed to Log out");
-		}
-	}
+    try {
+      await logout();
+      navigate("/");
+    } catch {
+      setError("Failed to Log out");
+    }
+  }
 
-	return (
-		<div className={classes.rootnav}>
-			<AppBar style={{ backgroundColor: "#4052b5", color: "white" }}>
-				<Toolbar>
-					<IconButton
-						edge="start"
-						className={classes.menuButton}
-						color="inherit"
-						aria-label="menu"
-						onClick={() => setOpenMenu(true)}
-					>
-						<MenuIcon />
-					</IconButton>
-					<IconButton
-						className={classes.title}
-						onClick={() => {
-							navigate("/");
-						}}
-					>
-						<HomeIcon />
-						Home
-					</IconButton>
-					<IconButton
-						className={classes.title}
-						onClick={() => {
-							navigate("/");
-							popularCocktailsSwitch();
-						}}
-					>
-						<img
-							alt="icon"
-							style={{ width: "45px", borderRadius: "30%" }}
-							src="/images/icon.png"
-						/>{" "}
-						Popular Cocktails
-					</IconButton>
-					<IconButton
-						className={classes.title}
-						onClick={() => {
-							navigate("/");
-							popularIngsSwitch();
-						}}
-					>
-						<img
-							alt="icon"
-							style={{ width: "60px", borderRadius: "30%" }}
-							src="https://thecocktaildb.com/images/ingredients/Baileys irish cream.png"
-						/>{" "}
-						Popular Ingredients
-					</IconButton>
-					<IconButton
-						style={{ background: "#4052b5" }}
-						className={classes.title}
-						onClick={() => {
-							navigate("/cocktail-builder");
-						}}
-					>
-						<img
-							alt="icon"
-							style={{ width: "60px", borderRadius: "30%" }}
-							src="/images/icon2.jpg"
-						/>{" "}
-						Cocktail Builder
-					</IconButton>
+  return (
+    <div className={classes.rootnav}>
+      <AppBar style={{ backgroundColor: "#4052b5", color: "white" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={() => setOpenMenu(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <IconButton
+            className={classes.title}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <HomeIcon />
+            Home
+          </IconButton>
+          <IconButton
+            className={classes.title}
+            onClick={() => {
+              navigate("/");
+              popularCocktailsSwitch();
+            }}
+          >
+            <img
+              alt="icon"
+              style={{ width: "45px", borderRadius: "30%" }}
+              src="/images/icon.png"
+            />{" "}
+            Popular Cocktails
+          </IconButton>
+          <IconButton
+            className={classes.title}
+            onClick={() => {
+              navigate("/");
+              popularIngsSwitch();
+            }}
+          >
+            <img
+              alt="icon"
+              style={{ width: "60px", borderRadius: "30%" }}
+              src="https://thecocktaildb.com/images/ingredients/Baileys irish cream.png"
+            />{" "}
+            Popular Ingredients
+          </IconButton>
+          <IconButton
+            style={{ background: "#4052b5" }}
+            className={classes.title}
+            onClick={() => {
+              navigate("/cocktail-builder");
+            }}
+          >
+            <img
+              alt="icon"
+              style={{ width: "60px", borderRadius: "30%" }}
+              src="/images/icon2.jpg"
+            />{" "}
+            Cocktail Builder
+          </IconButton>
 
-					<div className={classes.search}>
-						<div className={classes.searchIcon}>
-							<SearchIcon />
-						</div>
-						<InputBase
-							placeholder="Search…"
-							classes={{
-								root: classes.inputRoot,
-								input: classes.inputInput,
-							}}
-							inputProps={{ "aria-label": "search" }}
-						/>
-					</div>
-					<div>
-						<Typography variant="h6" className={classes.title}>
-							{`Hello : ${currentUser ? currentUser.displayName : "Guest"}`}
-						</Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <div>
+            <Typography variant="h6" className={classes.title}>
+              {`Hello : ${currentUser ? currentUser.displayName : "Guest"}`}
+            </Typography>
 
-						{currentUser ? (
-							<Typography variant="h6" className={classes.title}>
-								{" "}
-								{"Email : " + currentUser.email}
-							</Typography>
-						) : null}
-					</div>
-					<div>
-						{!currentUser ? (
-							<>
-								<Button
-									className={classes.title}
-									color="inherit"
-									onClick={() => {
-										navigate("/login");
-									}}
-								>
-									Login
-								</Button>
-								<Button
-									className={classes.title}
-									color="inherit"
-									onClick={() => {
-										navigate("/signup");
-									}}
-								>
-									Sign up
-								</Button>
-							</>
-						) : (
-							<>
-								<IconButton
-									className={classes.title}
-									aria-label="account of current user"
-									aria-controls="menu-appbar"
-									aria-haspopup="true"
-									onClick={handleMenu}
-									color="inherit"
-								>
-									<AccountCircle />
-								</IconButton>
-								<Menu
-									id="menu-appbar"
-									anchorEl={anchorEl}
-									anchorOrigin={{
-										vertical: "top",
-										horizontal: "right",
-									}}
-									keepMounted
-									transformOrigin={{
-										vertical: "top",
-										horizontal: "right",
-									}}
-									open={open}
-									onClose={handleClose}
-								>
-									<MenuItem onClick={handleClose}>
-										<ReactLink
-											style={{ color: "#4052b5" }}
-											to="/update-profile"
-										>
-											Update Profile
-										</ReactLink>
-									</MenuItem>
-									<MenuItem onClick={handleClose}>
-										<ReactLink
-											style={{ color: "#4052b5" }}
-											onClick={handleLogout}
-											to="/"
-										>
-											Log Out
-										</ReactLink>
-									</MenuItem>
-								</Menu>
-							</>
-						)}
-					</div>
-					<div>
-						{currentUser ? (
-							<IconButton
-								className={classes.title}
-								onClick={() => {
-									navigate("/shoping-card");
-								}}
-							>
-								<Badge
-									overlap="rectangular"
-									badgeContent={basketQty}
-									color="secondary"
-								>
-									<ShoppingCartIcon />
-								</Badge>
-							</IconButton>
-						) : (
-							""
-						)}
-					</div>
-				</Toolbar>
-			</AppBar>
-			{showDrawer && (<MenuDrawer
-				itemData={fetchData}
-				open={openMenu}
-				close={() => setOpenMenu(false)}
-			/>)}
-			
-		</div>
-	);
+            {currentUser ? (
+              <Typography variant="h6" className={classes.title}>
+                {" "}
+                {"Email : " + currentUser.email}
+              </Typography>
+            ) : null}
+          </div>
+          <div>
+            {!currentUser ? (
+              <>
+                <Button
+                  className={classes.title}
+                  color="inherit"
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  Login
+                </Button>
+                <Button
+                  className={classes.title}
+                  color="inherit"
+                  onClick={() => {
+                    navigate("/signup");
+                  }}
+                >
+                  Sign up
+                </Button>
+              </>
+            ) : (
+              <>
+                <IconButton
+                  className={classes.title}
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <ReactLink
+                      style={{ color: "#4052b5" }}
+                      to="/update-profile"
+                    >
+                      Update Profile
+                    </ReactLink>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <ReactLink
+                      style={{ color: "#4052b5" }}
+                      onClick={handleLogout}
+                      to="/"
+                    >
+                      Log Out
+                    </ReactLink>
+                  </MenuItem>
+                </Menu>
+              </>
+            )}
+          </div>
+          <div>
+            {currentUser ? (
+              <IconButton
+                className={classes.title}
+                onClick={() => {
+                  navigate("/shoping-card");
+                }}
+              >
+                <Badge
+                  overlap="rectangular"
+                  badgeContent={cartQty > 0 ? cartQty : null}
+                  color="secondary"
+                >
+                  <ShoppingCartIcon />
+                </Badge>
+              </IconButton>
+            ) : (
+              ""
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+      {showDrawer && (
+        <MenuDrawer
+          itemData={fetchData}
+          open={openMenu}
+          close={() => setOpenMenu(false)}
+        />
+      )}
+    </div>
+  );
 }
