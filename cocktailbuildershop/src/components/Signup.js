3a8a63b2ef506508f7c519be031559fb1e72314e
Samvel Tadevosyan <63152210+satadevosyan@users.noreply.github.com>
2022-05-12 00:43:48 +0400

    fix bug

--- cocktailbuildershop/src/components/Signup.js
+++ cocktailbuildershop/src/components/Signup.js
@@ -10,166 +10,163 @@ import Footer from "./Footer";
 import THEMES from "../consts/THEMES";
 
 export default function Signup() {
-	const classes = THEMES();
-	const emailRef = useRef();
-	const passwordRef = useRef();
-	const passwordConfirmRef = useRef();
-	const { signup, currentUser } = useContext(MainContext);
-	const [error, setError] = useState("");
-	const [show, setShow] = useState("password");
-	const navigate = useNavigate();
-	const nameRef = useRef();
-	const [, setLoading] = useState(true);
+  const classes = THEMES();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup, currentUser } = useContext(MainContext);
+  const [error, setError] = useState("");
+  const [show, setShow] = useState("password");
+  const navigate = useNavigate();
+  const nameRef = useRef();
+  const [, setLoading] = useState(true);
 
-	async function handleSubmit(e) {
-		e.preventDefault();
-		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-			return setError("Passwords do NOT match");
-		}
-		if (passwordRef.current.value.length < 6) {
-			return setError("Password must have at least 6 characters");
-		}
-		if (!emailRef.current.value.includes("@" || ".")) {
-			return setError("Invalid Email Format");
-		}
-		try {
-			setError("");
-			setLoading(true);
-			await signup(
-				emailRef.current.value,
-				passwordRef.current.value,
-				nameRef.current.value
-			).catch(() => {
-			});
-			navigate("/");
-		} catch {
-			setError("Failed to create an account");
-		}
-		setLoading(false);
-	}
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do NOT match");
+    }
+    if (passwordRef.current.value.length < 6) {
+      return setError("Password must have at least 6 characters");
+    }
+    if (!emailRef.current.value.includes("@" || ".")) {
+      return setError("Invalid Email Format");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await signup(
+        emailRef.current.value,
+        passwordRef.current.value,
+        nameRef.current.value
+      );
+      navigate("/");
+    } catch (e) {
+      setError("Failed to create an account");
+    }
+    setLoading(false);
+  }
 
-	return currentUser ? (
-		<Navigate to="/" />
-	) : (
-		<Container onSubmit={handleSubmit} component="main" maxWidth="xs">
-			<CssBaseline />
-			<div className={classes.paper}>
-				<Avatar className={classes.avatarred}>
-					<LockOutlinedIcon />
-				</Avatar>
-				<Typography component="h1" variant="h5">
-					Sign up
-				</Typography>
-				{error && (
-					<div className={classes.root}>
-						<Alert variant="filled" severity="error">
-							{error}
-						</Alert>
-					</div>
-				)}
-				<form className={classes.form} noValidate>
-					<Grid container spacing={2}>
-						<Grid item xs={12}>
-							<TextField
-								inputRef={nameRef}
-								autoComplete="fname"
-								name="Name"
-								variant="outlined"
-								required
-								fullWidth
-								id="Name"
-								label="Username"
-								autoFocus
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<TextField
-								type="email"
-								inputRef={emailRef}
-								variant="outlined"
-								required
-								fullWidth
-								id="email"
-								label="Email Address"
-								name="email"
-								autoComplete="email"
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<TextField
-								inputRef={passwordRef}
-								variant="outlined"
-								required
-								fullWidth
-								name="password"
-								label="Password"
-								type={show}
-								id="password"
-								autoComplete="current-password"
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<Grid item xs={12}>
-								<TextField
-									inputRef={passwordConfirmRef}
-									variant="outlined"
-									required
-									fullWidth
-									name="password"
-									label="Password Confirmation"
-									type={show}
-									id="password-confirm"
-									autoComplete="current-password"
-								/>
-							</Grid>
-							<FormControlLabel
-								control={
-									<Checkbox
-										onClick={() =>
-											setShow(show === "password" ? "text" : "password")
-										}
-										color="primary"
-									/>
-								}
-								label="Show passwords"
-							/>
-						</Grid>
-					</Grid>
-					<Button
-						type="submit"
-						fullWidth
-						variant="contained"
-						color="primary"
-						className={classes.submit}
-					>
-						Sign Up
-					</Button>
-					<Grid container justifyContent="flex-end">
-						<Grid item>
-							<Link
-								href="/login"
-								variant="body2"
-								style={{ paddingRight: "100px" }}
-							>
-								Already have an account? Sign in
-							</Link>
-						</Grid>
-					</Grid>
-					<div style={{ paddingLeft: "185px" }}>or</div>
-					<Grid container justifyContent="flex-end">
-						<Grid item>
-							<Link href="/" variant="body2" style={{ paddingRight: "185px" }}>
-								Cancel
-							</Link>
-						</Grid>
-					</Grid>
-				</form>
-			</div>
-			<Box mt={5}>
-				<Footer />
-			</Box>
-		</Container>
-	);
+  return currentUser ? (
+    <Navigate to="/" />
+  ) : (
+    <Container onSubmit={handleSubmit} component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatarred}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        {error && (
+          <div className={classes.root}>
+            <Alert variant="filled" severity="error">
+              {error}
+            </Alert>
+          </div>
+        )}
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                inputRef={nameRef}
+                autoComplete="fname"
+                name="Name"
+                variant="outlined"
+                required
+                fullWidth
+                id="Name"
+                label="Username"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                inputRef={emailRef}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                inputRef={passwordRef}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type={show}
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Grid item xs={12}>
+                <TextField
+                  inputRef={passwordConfirmRef}
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password Confirmation"
+                  type={show}
+                  id="password-confirm"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onClick={() =>
+                      setShow(show === "password" ? "text" : "password")
+                    }
+                    color="primary"
+                  />
+                }
+                label="Show passwords"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link
+                href="/login"
+                variant="body2"
+                style={{ paddingRight: "100px" }}
+              >
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+          <div style={{ paddingLeft: "185px" }}>or</div>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/" variant="body2" style={{ paddingRight: "185px" }}>
+                Cancel
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Footer />
+      </Box>
+    </Container>
+  );
 }
-
-
